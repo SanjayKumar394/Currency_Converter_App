eb21a26e7314f6a06ccda2aba29c247c1bc6c0d3
Sanjaykumar <sanjaykumar679593@gmail.com>
2024-12-21 20:08:29 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import sample from "./Sample";
+
+const App = () => {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [convertedAmount, setConvertedAmount] = useState(null);
+  const [exchangeRate, setExchangeRate] = useState(null);
+
+  useEffect(() => {
+    const getExchangeRate = async () => {
+      try {
+        let link = "https://api.exchangerate-api.com/v4/latest/";
+        let url = `${link}${fromCurrency}`;
+        const response = await axios.get(url);
+        // console.log(response);
+        setExchangeRate(response.data.rates[toCurrency]);
+      } catch (error) {
+        console.error("Error fetching from exchange rate: ", error);
+      }
+    };
+    getExchangeRate();
+  }, [fromCurrency, toCurrency]);
+
+  useEffect(() => {
+    if (exchangeRate !== null) {
+      setConvertedAmount((amount * exchangeRate).toFixed(2));
+    }
+  }, [amount, exchangeRate]);
+
+  const handleAmountChange = (e) => {
+    const value = parseFloat(e.target.value);
+    setAmount(isNaN(value) ? 0 : value);
+  };
+
+  const handleFromCurrencyChange = (e) => {
+    setFromCurrency(e.target.value);
+  };
+
+  const handleToCurrencyChange = (e) => {
+    setToCurrency(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="currency-converter">
+        <div className="box"></div>
+        <div className="data">
+          <h1>Currency converter</h1>
+          <div className="input-container">
+            <label htmlFor="amt">Amount: </label>
+            <input
+              type="number"
+              id="amt"
+              value={amount}
+              onChange={handleAmountChange}
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="fromCurrency">From Currency:</label>
+            <select
+              name=""
+              id="fromCurrency"
+              className="fromCurrency"
+              value={fromCurrency}
+              onChange={handleFromCurrencyChange}
+            >
+              <option value="USD">USD - United States Dollar</option>
+              <option value="EUR">EUR - Euro</option>I
+              <option value="GBP">GBP - British Pound Sterling</option>
+              <option value="JPY">JPY - Japanese Yen</option>
+              <option value="AUD">AUD - Australian Dollar</option>
+              <option value="CAD">CAD - Canadian Dollar</option>
+              <option value="CNY">CNY - Chinese Yuan</option>
+              <option value="INR">INR - Indian Rupee</option>
+              <option value="BRL">BRL - Brazilian Real</option>
+              <option value="ZAR">ZAR - South African Rand</option>
+            </select>
+          </div>
+          <div className="input-container">
+            <label htmlFor="toCurrency">To Currency:</label>
+            <select
+              name=""
+              id="toCurrency"
+              className="toCurrency"
+              value={toCurrency}
+              onChange={handleToCurrencyChange}
+            >
+              <option value="USD">USD - United States Dollar</option>
+              <option value="EUR">EUR - Euro</option>I
+              <option value="GBP">GBP - British Pound Sterling</option>
+              <option value="JPY">JPY - Japanese Yen</option>
+              <option value="AUD">AUD - Australian Dollar</option>
+              <option value="CAD">CAD - Canadian Dollar</option>
+              <option value="CNY">CNY - Chinese Yuan</option>
+              <option value="INR">INR - Indian Rupee</option>
+              <option value="BRL">BRL - Brazilian Real</option>
+              <option value="ZAR">ZAR - South African Rand</option>
+            </select>
+          </div>
+          <div className="result">
+            <p>
+              {amount} {fromCurrency} is equal to {convertedAmount} {toCurrency}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
